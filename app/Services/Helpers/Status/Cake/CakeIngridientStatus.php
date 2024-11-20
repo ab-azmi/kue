dd88e9eedba865aa6465b03308db9e45bb6780db
ab-azmi <azmi.kiva@gmail.com>
2024-11-20 14:07:07 +0800

    centralized error message on Error class constant

--- app/Services/Helpers/Status/Cake/CakeIngridientStatus.php
+++ app/Services/Helpers/Status/Cake/CakeIngridientStatus.php
@@ -1,37 +1,63 @@
 <?php
 
+use App\Services\Constant\Error;
 
 if (!function_exists("errAttachIngridients")) {
     function errAttachIngridients($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Attach Ingridients to Cake", $internalMsg, $status);
+        error(
+            Error::CAKE['ATTACH_INGRIDIENTS_FAILED']['code'],
+            Error::CAKE['ATTACH_INGRIDIENTS_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errDetachingIngridients")) {
     function errDetachingIngridients($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Detach Ingridients to Cake", $internalMsg, $status);
+        error(
+            Error::CAKE['DETACH_INGRIDIENTS_FAILED']['code'],
+            Error::CAKE['DETACH_INGRIDIENTS_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errDecrementingIngridientStock")) {
     function errDecrementingIngridientStock($internalMsg = "", $status = null)
     {
-        error(409, "Ingridient Out of Stock", $internalMsg, $status);
+        error(
+            Error::CAKE['DECREMENT_INGRIDIENT_STOCK_FAILED']['code'],
+            Error::CAKE['DECREMENT_INGRIDIENT_STOCK_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errCalculatingCOGS")) {
     function errCalculatingCOGS($internalMsg = "", $status = null)
     {
-        error(500, "Failed to calculate Cost Of Goods Sold (COGS)", $internalMsg, $status);
+        error(
+            Error::CAKE['CALCULATING_COGS_FAILED']['code'],
+            Error::CAKE['CALCULATING_COGS_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errCalculatingIngridientCost")) {
     function errCalculatingIngridientCost($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Calculate Total Cost of Ingridients", $internalMsg, $status);
+        error(
+            Error::CAKE['CALCULATING_INGRIDIENT_COST_FAILED']['code'],
+            Error::CAKE['CALCULATING_INGRIDIENT_COST_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
