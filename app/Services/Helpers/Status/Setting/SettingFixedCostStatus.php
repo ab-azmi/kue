908f9d0c70f10998e3864abf583ebcb0836b5a9f
ab-azmi <azmi.kiva@gmail.com>
2024-11-20 14:17:29 +0800

    centralized setting error status to Error constant

--- app/Services/Helpers/Status/Setting/SettingFixedCostStatus.php
+++ app/Services/Helpers/Status/Setting/SettingFixedCostStatus.php
@@ -1,29 +1,51 @@
 <?php
 
+use App\Services\Constant\Error;
+
 if (!function_exists("errCreateFixedCost")) {
     function errCreateFixedCost($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Create Fixed Cost", $internalMsg, $status);
+        error(
+            Error::SETTING['FIXEDCOST_CREATE_FAILED']['code'],
+            Error::SETTING['FIXEDCOST_CREATE_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errUpdateFixedCost")) {
     function errUpdateFixedCost($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Update Fixed Cost", $internalMsg, $status);
+        error(
+            Error::SETTING['FIXEDCOST_UPDATE_FAILED']['code'],
+            Error::SETTING['FIXEDCOST_UPDATE_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errDeleteFixedCost")) {
     function errDeleteFixedCost($internalMsg = "", $status = null)
     {
-        error(500, "Failed to Delete Fixed Cost", $internalMsg, $status);
+        error(
+            Error::SETTING['FIXEDCOST_DELETE_FAILED']['code'],
+            Error::SETTING['FIXEDCOST_DELETE_FAILED']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
 
 if (!function_exists("errGetFixedCost")) {
-    function errGetFixedCost($data = null, $status = null)
+    function errGetFixedCost($internalMsg = "", $status = null)
     {
-        success(404, "Fixed Cost Not Found!", $data, $status);
+        error(
+            Error::SETTING['FIXEDCOST_NOT_FOUND']['code'],
+            Error::SETTING['FIXEDCOST_NOT_FOUND']['msg'],
+            $internalMsg,
+            $status
+        );
     }
 }
