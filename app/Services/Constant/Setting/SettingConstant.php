bd8278b2fc404baacd1ff07568db9a6bf37f742b
ab-azmi <azmi.kiva@gmail.com>
2024-11-21 16:53:03 +0800

    make setting constant

---
+++ app/Services/Constant/Setting/SettingConstant.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Services\Constant\Setting;
+
+class SettingConstant
+{
+    const TAX_KEY = 'tax';
+    const PROFIT_MARGIN_KEY = 'profit_margin';
+
+    const OPTION = [
+        self::TAX_KEY,
+        self::PROFIT_MARGIN_KEY,
+    ];
+}
