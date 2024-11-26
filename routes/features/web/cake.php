0815d029e19a5ca3575819a944def9b5bfcd846d
ab-azmi <azmi.kiva@gmail.com>
2024-11-26 16:19:02 +0800

    remove prefix 'cakes' from route cakes

--- routes/features/web/cake.php
+++ routes/features/web/cake.php
@@ -8,37 +8,33 @@ use App\Http\Controllers\Web\Cake\CakeDiscountController;
 use App\Http\Controllers\Web\Cake\CakeVariantController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('cakes')
+
+Route::prefix('variants')
     ->group(function () {
-        Route::prefix('variants')
-            ->group(function () {
-                Route::get('', [CakeVariantController::class, 'get']);
-            });
-
-        Route::prefix('discounts')
-            ->group(function () {
-                Route::get('', [CakeDiscountController::class, 'get']);
-                Route::post('', [CakeDiscountController::class, 'create']);
-                Route::get('{id}', [CakeDiscountController::class, 'detail']);
-                Route::put('{id}', [CakeDiscountController::class, 'update']);
-                Route::delete('{id}', [CakeDiscountController::class, 'delete']);
-            });
-
-        Route::prefix('ingredients')
-            ->group(function () {
-                Route::get('', [CakeComponentIngredientController::class, 'get']);
-                Route::post('', [CakeComponentIngredientController::class, 'create']);
-                Route::get('{id}', [CakeComponentIngredientController::class, 'detail']);
-                Route::put('{id}', [CakeComponentIngredientController::class, 'update']);
-                Route::delete('{id}', [CakeComponentIngredientController::class, 'delete']);
-            });
-
-        Route::get('', [CakeController::class, 'get']);
-        Route::post('', [CakeController::class, 'create']);
-        Route::post('cogs', [CakeController::class, 'COGS']);
-        Route::get('{id}', [CakeController::class, 'detail']);
-        Route::put('{id}', [CakeController::class, 'update']);
-        Route::delete('{id}', [CakeController::class, 'delete']);
+        Route::get('', [CakeVariantController::class, 'get']);
     });
 
-Route::get('storage/cakes/{path}', [CakeController::class, 'getFile'])->withoutMiddleware('auth:api');
+Route::prefix('discounts')
+    ->group(function () {
+        Route::get('', [CakeDiscountController::class, 'get']);
+        Route::post('', [CakeDiscountController::class, 'create']);
+        Route::get('{id}', [CakeDiscountController::class, 'detail']);
+        Route::put('{id}', [CakeDiscountController::class, 'update']);
+        Route::delete('{id}', [CakeDiscountController::class, 'delete']);
+    });
+
+Route::prefix('ingredients')
+    ->group(function () {
+        Route::get('', [CakeComponentIngredientController::class, 'get']);
+        Route::post('', [CakeComponentIngredientController::class, 'create']);
+        Route::get('{id}', [CakeComponentIngredientController::class, 'detail']);
+        Route::put('{id}', [CakeComponentIngredientController::class, 'update']);
+        Route::delete('{id}', [CakeComponentIngredientController::class, 'delete']);
+    });
+
+Route::get('', [CakeController::class, 'get']);
+Route::post('', [CakeController::class, 'create']);
+Route::post('cogs', [CakeController::class, 'COGS']);
+Route::get('{id}', [CakeController::class, 'detail']);
+Route::put('{id}', [CakeController::class, 'update']);
+Route::delete('{id}', [CakeController::class, 'delete']);
