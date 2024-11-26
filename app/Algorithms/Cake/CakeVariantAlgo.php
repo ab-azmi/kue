b5287fb837aab931ccb48e4500d941ffff11d4a0
ab-azmi <azmi.kiva@gmail.com>
2024-11-26 17:10:26 +0800

    cake variant algo: make update and delete

--- app/Algorithms/Cake/CakeVariantAlgo.php
+++ app/Algorithms/Cake/CakeVariantAlgo.php
@@ -4,12 +4,12 @@ namespace App\Algorithms\Cake;
 
 use App\Models\Cake\CakeVariant;
 use App\Services\Constant\Activity\ActivityAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 
 class CakeVariantAlgo
 {
     /**
-     * CakeVariantAlgo constructor.
      * @param CakeVariant|int|null $cakeVariant
      */
     public function __construct(public CakeVariant|int|null $cakeVariant = null)
@@ -22,6 +22,11 @@ class CakeVariantAlgo
         }
     }
 
+    /**
+     * @param $request
+     *
+     * @return JsonResponse|mixed|void
+     */
     public function create($request)
     {
         try {
@@ -38,7 +43,49 @@ class CakeVariantAlgo
         }
     }
 
+    /**
+     * @param $request
+     *
+     * @return JsonResponse|mixed|void
+     */
+    public function update($request)
+    {
+        try{
+            DB::transaction(function() use ($request) {
+                $this->cakeVariant->setOldActivityPropertyAttributes(ActivityAction::UPDATE);
 
+                $this->saveCakeVariant($request);
+
+                $this->cakeVariant->setActivityPropertyAttributes(ActivityAction::UPDATE)
+                        ->saveActivity('Update Cake : '.$this->cakeVariant->id);
+            });
+
+            return success($this->cakeVariant);
+        }catch(\Exception $e) {
+            exception($e);
+        }
+    }
+
+    /**
+     * @return JsonResponse|mixed|void
+     */
+    public function delete()
+    {
+        try {
+            DB::transaction(function() {
+                $this->cakeVariant->setOldActivityPropertyAttributes(ActivityAction::DELETE);
+
+                $this->cakeVariant->delete();
+
+                $this->cakeVariant->setActivityPropertyAttributes(ActivityAction::DELETE)
+                        ->saveActivity('Delete Cake : '.$this->cakeVariant->id);
+            });
+
+            return success();
+        }catch(\Exception $e) {
+            exception($e);
+        }
+    }
 
     /** --- PRIVATE FUNCTION --- */
 
