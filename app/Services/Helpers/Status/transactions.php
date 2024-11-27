267edeecc91133c938a1b56ccd08c92d8ec22a56
ab-azmi <azmi.kiva@gmail.com>
2024-11-27 19:36:05 +0800

    refactor error status transactions

--- app/Services/Helpers/Status/transactions.php
+++ app/Services/Helpers/Status/transactions.php
@@ -1,112 +1,67 @@
 <?php
 
-use App\Services\Constant\Error;
-
 if (! function_exists('errTransactionCreate')) {
-    function errTransactionCreate($internalMsg = '', $status = null)
+    function errTransactionCreate($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['CREATE_FAILED']['code'],
-            Error::TRANSACTION['CREATE_FAILED']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Transaction Create Failed', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionUpdate')) {
-    function errTransactionUpdate($internalMsg = '', $status = null)
+    function errTransactionUpdate($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['UPDATE_FAILED']['code'],
-            Error::TRANSACTION['UPDATE_FAILED']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Transaction Update Failed', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionDelete')) {
-    function errTransactionDelete($internalMsg = '', $status = null)
+    function errTransactionDelete($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['DELETE_FAILED']['code'],
-            Error::TRANSACTION['DELETE_FAILED']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Transaction Delete Failed', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionGet')) {
-    function errTransactionGet($internalMsg = '', $status = null)
+    function errTransactionGet($internalMsg = '', $status = 404)
     {
-        error(
-            Error::TRANSACTION['NOT_FOUND']['code'],
-            Error::TRANSACTION['NOT_FOUND']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Transaction Not Found', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionTotalPrice')) {
-    function errTransactionTotalPrice($internalMsg = '', $status = null)
+    function errTransactionTotalPrice($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['FAILED_CALCULATE_TOTAL_PRICE']['code'],
-            Error::TRANSACTION['FAILED_CALCULATE_TOTAL_PRICE']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Failed to calculate total price', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionTotalDiscount')) {
-    function errTransactionTotalDiscount($internalMsg = '', $status = null)
+    function errTransactionTotalDiscount($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['FAILED_CALCULATE_TOTAL_DISCOUNT']['code'],
-            Error::TRANSACTION['FAILED_CALCULATE_TOTAL_DISCOUNT']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Failed to calculate total discount', $internalMsg);
     }
 }
 
 if (! function_exists('errTransactionTax')) {
-    function errTransactionTax($internalMsg = '', $status = null)
+    function errTransactionTax($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['FAILED_CALCULATE_TAX']['code'],
-            Error::TRANSACTION['FAILED_CALCULATE_TAX']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Failed to calculate tax', $internalMsg);
     }
 }
 
+
 /** --- TRANSACTION ORDER --- **/
+
 if (! function_exists('errOutOfStockOrder')) {
-    function errOutOfStockOrder($internalMsg = '', $status = null)
+    function errOutOfStockOrder($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['OUT_OF_STOCK_ORDER']['code'],
-            Error::TRANSACTION['OUT_OF_STOCK_ORDER']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Cake out of stock', $internalMsg);
     }
 }
 
 if (! function_exists('errCreateOrder')) {
-    function errCreateOrder($internalMsg = '', $status = null)
+    function errCreateOrder($internalMsg = '', $status = 500)
     {
-        error(
-            Error::TRANSACTION['ORDER_CREATE_FAILED']['code'],
-            Error::TRANSACTION['ORDER_CREATE_FAILED']['msg'],
-            $internalMsg,
-            $status
-        );
+        error($status, 'Failed to create Order', $internalMsg);
     }
 }
