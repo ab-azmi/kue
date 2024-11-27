a66f6d98b67b367db8e8d38ae66f5d59cf3d4c6f
ab-azmi <azmi.kiva@gmail.com>
2024-11-27 13:40:50 +0800

    change fixed-cost routes name

--- routes/features/web/setting.php
+++ routes/features/web/setting.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('settings')
     ->group(function () {
 
-        Route::prefix('fixed_costs')
+        Route::prefix('fixed-costs')
             ->group(function () {
                 Route::get('', [SettingFixedCostController::class, 'get']);
                 Route::post('', [SettingFixedCostController::class, 'create']);
